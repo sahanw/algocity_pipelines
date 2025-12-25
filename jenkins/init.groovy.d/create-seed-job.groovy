6ed085d9ec720cb0846f832ceb77fe1cf504692d
Sahan W <sahanw@users.noreply.github.com>
2025-12-25 11:41:16 +0000

    Fix seed job to work without credentials for public repo

--- jenkins/init.groovy.d/create-seed-job.groovy
+++ jenkins/init.groovy.d/create-seed-job.groovy
@@ -3,6 +3,7 @@ import hudson.model.FreeStyleProject
 import javaposse.jobdsl.plugin.*
 import hudson.plugins.git.GitSCM
 import hudson.plugins.git.BranchSpec
+import hudson.plugins.git.extensions.impl.CloneOption
 
 // Auto-create seed job that generates pipelines from YAML files
 def jenkins = Jenkins.instance
@@ -11,35 +12,41 @@ def jenkins = Jenkins.instance
 def seedJobName = "seed-job"
 def seedJob = jenkins.getItem(seedJobName)
 
-if (seedJob == null) {
-    println "Creating seed job..."
-    
-    // Create freestyle project
-    seedJob = jenkins.createProject(FreeStyleProject, seedJobName)
-    seedJob.setDescription("Auto-generated seed job that creates pipelines from applications/*.yaml files")
-    
-    // Configure Git SCM
-    def scm = new GitSCM("https://github.com/sahanw/algocity_pipelines.git")
-    scm.branches = [new BranchSpec("*/main")]
-    seedJob.setScm(scm)
-    
-    // Add Job DSL build step
-    def jobDslBuildStep = new ExecuteDslScripts()
-    jobDslBuildStep.setTargets("jenkins/jobs/create-pipelines.groovy")
-    jobDslBuildStep.setRemovedJobAction(RemovedJobAction.DELETE)
-    jobDslBuildStep.setRemovedViewAction(RemovedViewAction.DELETE)
-    jobDslBuildStep.setLookupStrategy(LookupStrategy.SEED_JOB)
-    
-    seedJob.buildersList.add(jobDslBuildStep)
-    seedJob.save()
-    
-    println "Seed job created successfully!"
-    
-    // Trigger the seed job to create all pipelines
-    println "Triggering seed job to create pipelines..."
-    seedJob.scheduleBuild2(0)
-    
-    println "Pipelines will be created automatically!"
-} else {
-    println "Seed job already exists, skipping creation"
+if (seedJob != null) {
+    println "Seed job already exists, deleting and recreating..."
+    seedJob.delete()
 }
+
+println "Creating seed job..."
+
+// Create freestyle project
+seedJob = jenkins.createProject(FreeStyleProject, seedJobName)
+seedJob.setDescription("Auto-generated seed job that creates pipelines from applications/*.yaml files")
+
+// Configure Git SCM without credentials (public repo)
+def scm = new GitSCM("https://github.com/sahanw/algocity_pipelines.git")
+scm.branches = [new BranchSpec("*/main")]
+
+// Add clone options to make it shallow and faster
+def cloneOption = new CloneOption(true, true, null, 1)
+scm.extensions.add(cloneOption)
+
+seedJob.setScm(scm)
+
+// Add Job DSL build step
+def jobDslBuildStep = new ExecuteDslScripts()
+jobDslBuildStep.setTargets("jenkins/jobs/create-pipelines.groovy")
+jobDslBuildStep.setRemovedJobAction(RemovedJobAction.DELETE)
+jobDslBuildStep.setRemovedViewAction(RemovedViewAction.DELETE)
+jobDslBuildStep.setLookupStrategy(LookupStrategy.SEED_JOB)
+
+seedJob.buildersList.add(jobDslBuildStep)
+seedJob.save()
+
+println "Seed job created successfully!"
+
+// Trigger the seed job to create all pipelines
+println "Triggering seed job to create pipelines..."
+seedJob.scheduleBuild2(0)
+
+println "Pipelines will be created automatically!"
